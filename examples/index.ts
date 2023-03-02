afc8435d5f0379e81e0725a39d6acdac247f75b2
PawelSiemieniuk <pawel.siemieniuk@softwarehut.com>
2023-03-02 15:44:05 +0100

    Edited index file

--- examples/index.ts
+++ examples/index.ts
@@ -1,17 +1,16 @@
 import { controllers } from './src/controlers';
 const { getRouter } = require('@softwarehutpl/express-zod-decorators');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(getRouter(controllers));
+
 app.get('/', (req: typeof express.Request, res: typeof express.Response) => {
   res.send('Express + TypeScript Server');
 });
-
-app.use(getRouter(controllers));
-app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log('️[server]: Server is running at http://localhost:3000');
